d4b04ca8d76de894a40b3c7c62f8a8064679e7d5
TranMyPhuAn <128568357+TranMyPhuAn@users.noreply.github.com>
2023-06-11 21:30:40 +0700

    tao du an

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  url: "mongodb://127.0.0.1:27017",
+  url: "mongodb+srv://anhtu:zxczxc123@go88.gjuheoc.mongodb.net/",
+  options: {
+    dbName: "BOPCLUB", //
+    //'dbName': 'GAME', //
+    //'dbName': 'admin', //
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //'autoIndex':       false,
+  },
+};
