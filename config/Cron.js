768c7745e5afdcf09ec5b4f9bc846f6fb24013c1
TranMyPhuAn <128568357+TranMyPhuAn@users.noreply.github.com>
2023-06-13 22:38:47 +0700

    vps moi

--- config/Cron.js
+++ config/Cron.js
@@ -3,38 +3,6 @@ let CronHu = require("../app/Cron/EventHu");
 let XSMB = require("../app/Cron/XSMB/XSMB");
 let XSMB_trathuong = require("../app/Cron/XSMB/XSMB_trathuong");
 
-// module.exports = function () {
-//   new CronJob(
-//     "0 0 0 * * *",
-//     function () {
-//       CronHu();
-//     },
-//     null,
-//     true,
-//     "Asia/Ho_Chi_Minh"
-//   );
-
-//   new CronJob(
-//     "0 * 18 * * *",
-//     function () {
-//       XSMB();
-//     },
-//     null,
-//     true,
-//     "Asia/Ho_Chi_Minh"
-//   );
-
-//   new CronJob(
-//     "0 35 18 * * *",
-//     function () {
-//       XSMB_trathuong();
-//     },
-//     null,
-//     true,
-//     "Asia/Ho_Chi_Minh"
-//   );
-// };
-
 module.exports = function () {
   new CronJob(
     "0 0 0 * * *",
@@ -43,7 +11,7 @@ module.exports = function () {
     },
     null,
     true,
-    "Asia/Singapore"
+    "Asia/Ho_Chi_Minh"
   );
 
   new CronJob(
@@ -53,7 +21,7 @@ module.exports = function () {
     },
     null,
     true,
-    "Asia/Singapore"
+    "Asia/Ho_Chi_Minh"
   );
 
   new CronJob(
@@ -63,6 +31,20 @@ module.exports = function () {
     },
     null,
     true,
-    "Asia/Singapore"
+    "Asia/Ho_Chi_Minh"
   );
 };
+
+// module.exports = function() {
+// 	new CronJob('0 0 0 * * *', function() {
+// 	  CronHu();
+// 	}, null, true, 'Asia/Singapore');
+
+// 	new CronJob('0 * 18 * * *', function() {
+// 	  XSMB();
+// 	}, null, true, 'Asia/Singapore');
+
+// 	new CronJob('0 35 18 * * *', function() {
+// 	  XSMB_trathuong();
+// 	}, null, true, 'Asia/Singapore');
+//   }
