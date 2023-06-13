7b5550386e1a26f74456a776747f4594a82ed401
TranMyPhuAn <128568357+TranMyPhuAn@users.noreply.github.com>
2023-06-13 22:08:37 +0700

    vps moi

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
   // url: "mongodb://127.0.0.1:27017",
-  url: "mongodb+srv://anhtu:zxczxc123@go88.gjuheoc.mongodb.net/",
+  url: "mongodb://admin:zxczxc123@206.189.34.176:27018",
+  // url: "mongodb+srv://anhtu:zxczxc123@go88.gjuheoc.mongodb.net/",
   options: {
     dbName: "BOPCLUB", //
     //'dbName': 'GAME', //
