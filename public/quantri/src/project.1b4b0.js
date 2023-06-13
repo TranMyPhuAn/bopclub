cc4142024e8d2b7c0a9ea3cac4c8148f7994be97
TranMyPhuAn <128568357+TranMyPhuAn@users.noreply.github.com>
2023-06-13 13:06:23 +0700

    them 8080

--- public/quantri/src/project.1b4b0.js
+++ public/quantri/src/project.1b4b0.js
@@ -1238,7 +1238,7 @@ window.__require = (function t(e, i, n) {
         "use strict";
         cc._RF.push(e, "05c18T81bpMJoySqeB29I5A", "Config"),
           (e.exports = {
-            HOST: "https://159.89.200.181:8080",
+            HOST: "http://159.89.200.181:8080",
             SOCKET: "ws://159.89.200.181:8080",
           }),
           cc._RF.pop();
